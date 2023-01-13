22a7a09908d9761cda371384ccd7d0f0ce66ee67
Oscar <ismael_pascual@hotmail.com>
2023-01-12 23:39:56 -0800

    Initial Commit

---
+++ FirstApp/ByeView.swift
@@ -0,0 +1,21 @@
+//
+//  ByeView.swift
+//  FirstApp
+//
+//  Created by oscar rodriguez on 1/12/23.
+//
+
+import SwiftUI
+
+struct ByeView: View {
+    var body: some View {
+        Text("Bye bye view")
+    }
+}
+
+struct ByeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ByeView()
+    }
+}
+
---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by oscar rodriguez on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
