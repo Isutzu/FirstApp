51ac11264b609cdda0726f2ee357572f75233ecc
Oscar <ismael_pascual@hotmail.com>
2023-01-13 00:22:21 -0800

    first commit

--- FirstApp/ByeView.swift
+++ FirstApp/ByeView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ByeView: View {
     var body: some View {
-        Text("Bye bye view")
+        VStack{
+            Text("Hello Again")
+        }
     }
 }
 
--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -9,18 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        VStack {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundColor(.accentColor)
-//            Text("Hello")
-//        }
-        ByeView()
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello")
+        }
+        //ByeView()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .previewDevice("iPhone SE (3rd generation)")
     }
 }
